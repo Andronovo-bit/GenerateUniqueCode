2e52a0f6f2986861e5adcea483ae52f431d479af
Seyyid Yiğit <seyyid364@gmail.com>
2024-07-28 16:42:14 +0300

    feat: Added Domain project to solution.
    -Created CampaingCodeSettings for able to read campaing code settings in appsettings.json file.

---
+++ src/Domain/CampaignCodeSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace GenerateCampaignCode.Domain.Entities;
+
+public class CampaignCodeSettings
+{
+    public string Characters { get; set; }
+    public int Length { get; set; }
+    public string? PrivateKey { get; set; }
+}
